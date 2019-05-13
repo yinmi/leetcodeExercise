9c120402855a3dcb6b3af37662ba5b1450fae34f
yinmi <25746752+yinmi@users.noreply.github.com>
2019-05-13 22:53:28 +0800

    Update 27.移除元素.cs
    移除元素

--- 27.移除元素.cs
+++ 27.移除元素.cs
@@ -5,7 +5,20 @@
  */
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-            
+      int length=nums.Length;
+        for (int i=0;i<length;)
+        {
+            if(nums[i]==val)
+            {
+                nums[i]=nums[length-1];
+                length--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return length;      
     }
 }
 
