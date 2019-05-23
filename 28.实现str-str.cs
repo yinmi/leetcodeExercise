7efe057cfafb3508f5194ddf9533c3c2c40cf98c
yinmi <chunhuang91@gmail.com>
2019-05-23 23:00:03 +0800

    KMP算法

--- 28.实现str-str.cs
+++ 28.实现str-str.cs
@@ -1,46 +1,62 @@
 /*
  * @lc app=leetcode.cn id=28 lang=csharp
- *
+ *KPM
  * [28] 实现strStr()
  */
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        if(needle=="")
-        {
-            return 0;
-        }
-        char[] haystackArray=haystack.ToCharArray();
-        char[] needleArray=needle.ToCharArray();
-        int num=-1;
-        /*
-        找出第一个字符所在位置
-         */
-         for(int j=0;j<haystackArray.Length;j++)
-        {
-                if(haystackArray[j]==needleArray[0])
-               {
-                  num=j; 
-                  break;
-               } 
+        if(haystack==""&&needle=="")
+        return 0;
+        if(haystack==""&&needle!="")
+        return -1;
+         if(haystack!=""&&needle=="")
+        return 0;
+     char [] haystackArray=haystack.ToCharArray();
+     char [] needleArray=needle.ToCharArray();
+     int [] next=new int[needle.Length];
+     nextSolution(next,needleArray);
+     int i=0,j=0;
+     for(;i<needleArray.Length&&j<haystack.Length;)
+     {
+         if(needleArray[i]==haystackArray[j])
+         {
+             i++;j++;
+         }else if(next[i]>=0)
+         {
+             i=next[i];
+
+         }else
+         {
+             i=0;j++;
          }
-         /*判断后续字符是否相同 */
-             for(int k=1;k<needleArray.Length;k++)
+     }
+     if(i>=needle.Length)
+     {
+         return j-needle.Length;
+     }else
+     {
+         return -1;
+     }
+
+    }
+    private void nextSolution(int [] next,char[] B)
+    {
+        next[0]=-1;
+        for(int i=1;i<next.Length;i++)
+        {
+            int j=next[i-1];
+            while((B[j+1]!=B[i]&&(j>=0)))
             {
-                if(num+k>=haystackArray.Length)
-                {
-                    num=-1;
-                    break;
-                }
-                
-                if(haystackArray[num+k]!=needleArray[k])
-                {
-                    num=-1;
-                    break;
-                }
-
-             }
-        return num;
-
+                j=next[j];
+            }
+            if(B[j+1]==B[i])
+            {
+                next[i]=j+1;
+            }else
+            {
+                next[i]=-1;
+            }
+        }
     }
 }
 
